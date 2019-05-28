d1b10aceb5c5a10156b04abb0ac2e7b5f299bf8f
Ewelina Backtrog <oyfg@o2.pl>
2019-05-28 09:29:39 +0200

    add widgets

--- script1.py
+++ script1.py
@@ -2,5 +2,13 @@ from tkinter import *
 
 window=Tk()
 
+b1=Button(window, text="Button")
+b1.grid(row=0,column=0)
+
+e1=Entry(window)
+e1.grid(row=0,column=1)
+
+t1=Text(window, heigh=1, width=25)
+t1.grid(row=0,column=2)
 
 window.mainloop()
