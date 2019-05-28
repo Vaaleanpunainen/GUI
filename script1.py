0679a60f76dcb1637732fb32867a0035b8bdfa2b
Ewelina Backtrog <oyfg@o2.pl>
2019-05-28 09:59:06 +0200

    change code
    # print out the user entry in the text area and command line

--- script1.py
+++ script1.py
@@ -4,6 +4,7 @@ window=Tk()
 
 def print_entry():
     print(e1_value.get())
+    t1.insert(END,e1_value.get())
 
 b1=Button(window, text="Execute", command=print_entry)
 b1.grid(row=0,column=0)
@@ -12,7 +13,7 @@ e1_value=StringVar()
 e1=Entry(window, textvariable=e1_value)
 e1.grid(row=0,column=1)
 
-t1=Text(window, heigh=1, width=25)
+t1=Text(window, heigh=25, width=25)
 t1.grid(row=0,column=2)
 
 window.mainloop()
